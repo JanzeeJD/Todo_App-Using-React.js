9d44c9ec8af6937da9ee6e6828953a1704340c26
Jancy Daniel <jancyjaya12@gmail.com>
2024-08-18 16:59:47 +0530

    chore: remove unused uuidv4 call

--- my-react-app/src/Components/TodoWrapper.jsx
+++ my-react-app/src/Components/TodoWrapper.jsx
@@ -3,7 +3,7 @@ import { TodoForm } from './TodoForm'
 import {v4 as uuidv4} from 'uuid';
 import { Todo } from './Todo';
 import { EditTodoForm } from './EditTodoForm';
-uuidv4();
+
 
 export const TodoWrapper = ()=> {
   const [todos, setTodos] = useState([])
