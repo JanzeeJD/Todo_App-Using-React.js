883383d40cf77d25b8f7f175d7c91dcddceabbfe
Jancy Daniel <jancyjaya12@gmail.com>
2024-09-01 10:00:00 +0530

    chore: disable autocomplete in task input

--- my-react-app/src/Components/TodoForm.jsx
+++ my-react-app/src/Components/TodoForm.jsx
@@ -12,7 +12,7 @@ export const TodoForm = ({addTodo})=>{
   }
   return(
     <form className="TodoForm" onSubmit={handleSubmit}>
-      <input type="text" className="todo-input" value={value}
+      <input type="text" className="todo-input" autoComplete="off" value={value}
       placeholder="What is the task today?" onChange={handleChange}/>
       <button type="submit" className="todo-btn">Add Task</button>
     </form>
